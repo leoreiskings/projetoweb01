11c748526a8471aa91d3812f6a5910c1a30cd565
leoreiskings <leo19reis@gmail.com>
2023-11-01 16:06:53 -0300

    omissão de senha de acesso ao BD

--- src/main/java/br/com/kingsdevs/factories/ConnectionFactory.java
+++ src/main/java/br/com/kingsdevs/factories/ConnectionFactory.java
@@ -10,7 +10,7 @@ public class ConnectionFactory {
 	
 		String url = "jdbc:mysql://localhost:3306/projetoweb01?useTimezone=true&serverTimezone=UTC&useSSL=false";
 		String user = "root";
-		String password = "reis";
+		String password = "";
 		String driver = "com.mysql.jdbc.Driver";
 	
 		Class.forName(driver);
